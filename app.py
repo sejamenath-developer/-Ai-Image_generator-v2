d733477f805e5f1fa693f26c9005452ee28351ee
Smenath.development <154619913+sejamenath-developer@users.noreply.github.com>
2024-01-05 16:26:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import subprocess
 
 # Install pygit2 with the specified version
-subprocess.run(['pip', 'install', 'pygit2==1.12.2'])
+install_pygit2 = subprocess.run(['pip', 'install', 'pygit2==1.12.2'], capture_output=True, text=True)
 
 # Change to the desired directory
-subprocess.run(['cd', '/content'])
+change_directory = subprocess.run(['cd', '/content'], shell=True, capture_output=True, text=True)
 
 # Clone the repository from GitHub
-subprocess.run(['git', 'clone', 'https://github.com/lllyasviel/Fooocus.git'])
+clone_repository = subprocess.run(['git', 'clone', 'https://github.com/lllyasviel/Fooocus.git'], capture_output=True, text=True)
 
 # Change to the cloned repository directory
-subprocess.run(['cd', '/content/Fooocus'])
+change_to_cloned_dir = subprocess.run(['cd', '/content/Fooocus'], shell=True, capture_output=True, text=True)
 
 # Run the Python script with the --share argument
-subprocess.run(['python', 'entry_with_update.py', '--share'])
+run_script = subprocess.run(['python', 'entry_with_update.py', '--share'], capture_output=True, text=True)
+
+# Print the deployment link or output from the script
+print(run_script.stdout)
