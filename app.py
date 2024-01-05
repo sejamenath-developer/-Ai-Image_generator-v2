65fe1d6e136637e292fb294f5b52d4dacec013d5
Smenath.development <154619913+sejamenath-developer@users.noreply.github.com>
2024-01-05 17:10:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-!pip install pygit2==1.12.2
-%cd /content
-!git clone https://github.com/lllyasviel/Fooocus.git
-%cd /content/Fooocus
-!python entry_with_update.py --share
+import subprocess
+
+# Install pygit2 version 1.12.2
+subprocess.run(["pip", "install", "pygit2==1.12.2"])
+
+# Change directory to /content
+subprocess.run(["cd", "/content"], shell=True)
+
+# Clone the repository from GitHub
+subprocess.run(["git", "clone", "https://github.com/lllyasviel/Fooocus.git"])
+
+# Change directory to /content/Fooocus
+subprocess.run(["cd", "/content/Fooocus"], shell=True)
+
+# Run the Python script entry_with_update.py with the --share flag
+subprocess.run(["python", "entry_with_update.py", "--share"])
